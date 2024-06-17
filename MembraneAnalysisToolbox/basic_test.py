2bfb7879f986733789d72c00623119f71d215307
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-06-17 10:05:55 +0200

    test initial commit

---
+++ MembraneAnalysisToolbox/basic_test.py
@@ -0,0 +1,16 @@
+import unittest
+# write unittests for python
+# now this is just so a test ist there as a framework, no actual test happening yet
+
+
+class TestTFM(unittest.TestCase):
+    def test_upper(self):
+        sum = 3 + 3
+        self.assertEqual(sum, 6)
+
+    def hor_dist(self):
+        self.assertEqual(0, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()

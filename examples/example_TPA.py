5ba93f8eea45e35d07958ce9bf3483b9b7b9d16f
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-06-22 18:41:23 +0200

    refactor: prettier print statements

--- examples/example_TPA.py
+++ examples/example_TPA.py
@@ -1,8 +1,15 @@
 import MembraneAnalysisToolbox.TransitionPathAnalysis as TPA
 import matplotlib.pyplot as plt
 
-# TODO actually create the test xtc and topology file
-path = "./tests/"
+"""
+This example shows how to use the TransitionPathAnalysis class to analyse the transitions of solvents through a metal organic framework (MOF) membrane.
+
+TODO:
+    - create the test xtc and topology file as small sample from a big xtc file
+"""
+
+path = "./tests/data/"
+print("Path: " + path)
 
 # STEP 1: initialise the Data into the class
 Analysis2nm_1 = TPA.TransitionPathAnalysis( 
@@ -14,22 +21,24 @@ L = 180
 
 # STEP 2: Find z_lower and validate by eye
 z_lower = Analysis2nm_1.find_z_lower_hexstruc(mem_selector = "resname C", L=L)
-print("z_lower: " + str(z_lower))
+print("Analysis of the membrane z-dimension")
+print("\tz_lower: " + str(z_lower))
 Analysis2nm_1.inspect(["resname HEX and name C1"], z_lower, L)
 
 # STEP 3: analyse passage times and calculate diffusion coefficient
 # Hex analysis
-print("Hex analysis")
+print("\nHEX analysis")
 ffs, ffe, indizes = Analysis2nm_1.calc_passagetimes(["resname HEX and name C1"], z_lower, L)
-print("passages: " + str(len(ffs)))
+print("\tpassages: " + str(len(ffs)))
 D = Analysis2nm_1.calc_diffusion(list(ffe-ffs), L, T = 296)
-print("Diffusioncoefficient: " + str(D))
+print("\tDiffusioncoefficient: " + str(D).replace(".",","))
 plt.show()
 
 # Dod analysis
-print("Dod analysis")
+print("\nDOD analysis")
 ffs, ffe, indizes = Analysis2nm_1.calc_passagetimes(["resname DOD and name C2"], z_lower, L)
-print("passages: " + str(len(ffs)))
+print("\tpassages: " + str(len(ffs)))
 D = Analysis2nm_1.calc_diffusion(list(ffe-ffs), L, T = 296)
-print("Diffusioncoefficient: " + str(D))
+print("\tDiffusioncoefficient: " + str(D).replace(".",","))
 plt.show()
+print("\n\n\n")

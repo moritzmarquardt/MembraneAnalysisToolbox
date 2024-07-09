760b09b974f3e10524e22bf7b9bd0504f9758de0
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-07-09 18:50:48 +0200

    doc: add documentation

---
+++ src/MembraneAnalysisToolbox/__init__.py
@@ -0,0 +1,8 @@
+"""
+Module containing class objects for analysing membrane simulations.
+
+This module contains the following classes:
+- MembraneAnalysis: A class for analysing membrane simulations.
+- DiffusionAnalysis: A class for analysing the diffusion of molecules in the membrane.
+- EffectivePoreSizeAnalysis: A class for analysing the effective pore size of the membrane.
+"""

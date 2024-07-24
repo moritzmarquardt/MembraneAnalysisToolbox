f44028f584ff5e83060cc945b2dc0852a57db56f
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-07-24 13:50:08 +0200

    refactor!: add plots and move diffusion calculation to core_functions

--- src/MembraneAnalysisToolbox/DiffusionAnalysis.py
+++ src/MembraneAnalysisToolbox/DiffusionAnalysis.py
@@ -6,7 +6,13 @@ import numpy as np
 from scipy.optimize import least_squares
 from statsmodels.distributions.empirical_distribution import ECDF
 
-from MembraneAnalysisToolbox.core_functions import dur_dist_improved, save_1darr_to_txt
+from MembraneAnalysisToolbox.core_functions import (
+    calculate_diffusion,
+    dur_dist_improved,
+    fitfunc_hom,
+    fitfunc_hom_cdf,
+    save_1darr_to_txt,
+)
 from MembraneAnalysisToolbox.MembraneAnalysis import MembraneAnalysis
 
 
@@ -50,6 +56,7 @@ class DiffusionAnalysis(MembraneAnalysis):
         self.z_lower = z_lower
         self.passageTimes = {}
         self.passageStarts = {}
+        self.passageIndices = {}
         self.n_passages = {}
         self.D = {}
 
@@ -68,6 +75,7 @@ class DiffusionAnalysis(MembraneAnalysis):
             f"  D: {self.D}\n"
             f"  passageTimes: {self.passageTimes}\n"
             f"  passageStarts: {self.passageStarts}\n"
+            f"  passageIndices: {self.passageIndices}\n"
             f"  n_passages: {self.n_passages}\n"
             f"  results_dir: {self.results_dir}\n"
             f"  topology_file: {self.topology_file}\n"
@@ -114,7 +122,7 @@ class DiffusionAnalysis(MembraneAnalysis):
 
         z = self.trajectories[selector][:, :, 2]
         z_boundaries = [self.z_lower, self.z_lower + self.L]
-        ffs, ffe, _ = dur_dist_improved(
+        ffs, ffe, ffi = dur_dist_improved(
             z, z_boundaries
         )  # only calcs the timesteps, not the time
 
@@ -125,6 +133,7 @@ class DiffusionAnalysis(MembraneAnalysis):
 
         self.passageTimes[selector] = ffe_ns - ffs_ns
         self.passageStarts[selector] = ffs_ns
+        self.passageIndices[selector] = ffi
         self.n_passages[selector] = len(ffs_ns)
 
     # TODO correct implementation of this function
@@ -144,20 +153,15 @@ class DiffusionAnalysis(MembraneAnalysis):
             raise ValueError(
                 "L must be set before calculating the diffusion coefficient."
             )
+
         if selector not in self.passageTimes.keys():
             raise ValueError(
                 "Passage times for the selector must be calculated before calculating the diffusion coefficient."
             )
 
         passage_times = self.passageTimes[selector] / 1000  # convert to ns
-        ecdf = ECDF(passage_times)
 
-        # FIT DATA
-        params_hom_cdf = self.fitting_hom_cdf_lsq(ecdf.x[1:], ecdf.y[1:], self.L)
-
-        D_hom_cdf = params_hom_cdf[0]
-
-        self.D[selector] = D_hom_cdf
+        self.D[selector] = calculate_diffusion(self.L, passage_times)
 
     def plot_diffusion(self, selector: str):
         if self.L is None:
@@ -183,7 +187,7 @@ class DiffusionAnalysis(MembraneAnalysis):
         """ PLOT DATA """
         x_lim = centertime * 4
         x_cdf = np.linspace(0, x_lim * 2, 200)
-        y_hom_cdf = self.fitfunc_hom_cdf(x_cdf, D, self.L)
+        y_hom_cdf = fitfunc_hom_cdf(x_cdf, D, self.L)
 
         fig, (ax1, ax2) = plt.subplots(2)
         fig.suptitle("PDF and CDF fit")
@@ -202,7 +206,7 @@ class DiffusionAnalysis(MembraneAnalysis):
 
         """ PLOT DATA """
         x = x_cdf
-        y2 = self.fitfunc_hom(x, D, self.L)
+        y2 = fitfunc_hom(x, D, self.L)
 
         ax1.hist(passage_times, bins=len(center), density=True, color=[0, 0.5, 0.5])
         ax1.plot(x[1:], y2[1:], label="hom", color="red", ls="dashed")
@@ -304,40 +308,59 @@ class DiffusionAnalysis(MembraneAnalysis):
             json.dump(dictionary, f)
 
     # TODO implement this function correctly
-    def plot_rand_passages(ax, selector: str, rand_n: int):
-        """kernel_size = 1000
-        kernel = np.ones(kernel_size) / kernel_size
+    def create_rand_passages_plot(self, selector: str, n: int) -> tuple:
+        self._allocateTrajectories(selector)
+
         fig = plt.figure("3d trajektorien")
         ax = fig.add_subplot(projection="3d")
-        for sel in np.random.randint(0, x_passages.shape[0], size=(3)):
-            print(sel)
-            x_passages_sel = x_passages[sel]
-            y_passages_sel = y_passages[sel]
-            z_passages_sel = z_passages[sel]
-            slicer = slice(ffs[sel] - 1, ffe[sel] + 2, 1)
+
+        print(self.passageIndices[selector].shape[0])
+        rand_indices = np.random.randint(
+            0, self.passageIndices[selector].shape[0], size=(n)
+        )
+        print(rand_indices)
+        # transform ns back to frames by dividing by the step size
+        ffss = self.passageStarts[selector][rand_indices] / self.step_size
+        print(ffss)
+        ffss = ffss.astype(int)
+        print(ffss)
+        ffes = ffss + (self.passageTimes[selector][rand_indices] / self.step_size)
+        print(ffes)
+        ffes = ffes.astype(int)
+        print(ffes)
+        ffis = self.passageIndices[selector][rand_indices]
+        print(ffis)
+
+        x_passages = self.trajectories[selector][ffis, :, 0]
+        y_passages = self.trajectories[selector][ffis, :, 1]
+        z_passages = self.trajectories[selector][ffis, :, 2]
+
+        for sel in range(n):
+            slicer = slice(ffss[sel] - 1, ffes[sel] + 2, 1)
+            x_passages_sel = x_passages[sel, slicer]
+            y_passages_sel = y_passages[sel, slicer]
+            z_passages_sel = z_passages[sel, slicer]
             ax.plot(
-                ax,
-                x_passages_sel[slicer],
-                y_passages_sel[slicer],
-                z_passages_sel[slicer],
+                x_passages_sel,
+                y_passages_sel,
+                z_passages_sel,
             )  # ++1 so that the last timestep is also included
-            ax.scatter(
-                ax,
-                x_passages_sel[ffs[sel] - 1],
-                y_passages_sel[ffs[sel] - 1],
-                z_passages_sel[ffs[sel] - 1],
-            )  # starting point
-            ax.scatter(
-                ax,
-                x_passages_sel[ffe[sel] + 2],
-                y_passages_sel[ffe[sel] + 2],
-                z_passages_sel[ffe[sel] + 2],
-            )  # end point"""
-        pass
+        ax.scatter(
+            x_passages[:, 0],
+            y_passages[:, 0],
+            z_passages[:, 0],
+        )  # starting point
+        ax.scatter(
+            x_passages[:, -1],
+            y_passages[:, -1],
+            z_passages[:, -1],
+        )  # end point
+        return fig, ax
 
     # TODO: implement this function correctly
-    def plot_starting_points(ax, X, Y, Z):
-        """fig = plt.figure("plotten aller Startpunkte")
+    def plot_starting_points(self, selector: str):
+        res = selector.split(" ")[1]
+        fig = plt.figure("plotten aller Startpunkte")
         ax = fig.add_subplot(projection="3d")
         ax.set_xlabel("x in nm", fontsize="x-large")
         ax.set_ylabel("y in nm", fontsize="x-large")
@@ -346,56 +369,14 @@ class DiffusionAnalysis(MembraneAnalysis):
             "Membrane-entry points of the passage-trajectories (" + res.upper() + ")",
             fontsize="x-large",
         )
+        x_passages = self.trajectories[selector][self.passageIndices[selector], :, 0]
+        y_passages = self.trajectories[selector][self.passageIndices[selector], :, 1]
+        z_passages = self.trajectories[selector][self.passageIndices[selector], :, 2]
+        ffs = self.passageStarts[selector] / self.step_size
+        ffs = ffs.astype(int)
         ax.scatter(
             x_passages[np.arange(np.size(x_passages, 0)), ffs + 1] / 10,
             y_passages[np.arange(np.size(x_passages, 0)), ffs + 1] / 10,
             z_passages[np.arange(np.size(x_passages, 0)), ffs + 1] / 10,
         )  # ugly way of getting the point. maybe there is a better way"""
-        pass
-
-    #########################################################################################
-    # Funktionen aus Gottholds Skript #######################################################
-    def hom_cdf(self, x, D, i, L):
-        t = (L) ** 2 / (i**2 * np.pi**2 * D)  # L^2/(i^2*pi^2*D)
-        return (-1) ** (i - 1) * np.exp(-x / t)  # summand in Gl. 10 vanHijkoop
-
-    def fitfunc_hom_cdf(self, x, D, L):
-        i = 50  # Summe geht bis 50 (approx statt undendlich)
-        result = 0
-        for j in range(1, i):
-            result = result + self.hom_cdf(x, D, j, L)
-        return 1 - 2 * result  # gleichung 10 in vanHijkoop paper
-
-    def fitfunc_hom_cdf_lsq(self, L):
-        def f(D, x, y):
-            i = 50
-            result = 0
-            for j in range(1, i):
-                result = result + self.hom_cdf(x, D, j, L)
-            return 1 - 2 * result - y
-
-        return f
-
-    def fitting_hom_cdf_lsq(self, x_data, y_data, L):
-        res_robust = least_squares(
-            self.fitfunc_hom_cdf_lsq(L),
-            x0=20,
-            loss="soft_l1",
-            f_scale=0.3,
-            args=(x_data, y_data),
-        )
-        return res_robust.x
-
-    def fitfunc_hom(self, x, D, L):
-        i = 151
-        result = 0
-        for j in range(1, i):
-            result = result + self.hom(x, D, j, L)
-        return 2 * np.pi**2 * D / (L) ** 2 * result
-
-    def hom(self, x, D, i, L):
-        t = (L) ** 2 / (i**2 * np.pi**2 * D)
-        return (-1) ** (i - 1) * i**2 * np.exp(-x / t)
-
-    # Ende Funktionen aus Gottholds Skript ##################################################
-    #########################################################################################
+        return fig, ax
